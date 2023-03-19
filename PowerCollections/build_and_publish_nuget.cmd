ce0884c3627f798a9dd9a67bf992698065897549
Nikita Strekalov (laptop) <nikita.strekalov123@gmail.com>
2023-03-19 19:49:11 +0400

    add build publish pakage and test

---
+++ PowerCollections/build_and_publish_nuget.cmd
@@ -0,0 +1,2 @@
+dotnet pack
+dotnet nuget push "PowerCollections\bin\Debug\*.nupkg" -k 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 -s https://nuget.pkg.github.com/nikita632112/index.json --skip-duplicate
---
+++ Task_01_Console_App/Task_01_Console_App/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+PowerCollections.Stack<int> stack = new PowerCollections.Stack<int>();
+Console.WriteLine(stack.Count);
+stack.Push(0);
+stack.Push(1);
+stack.Push(2);
+Console.WriteLine(stack.Count);
+Console.WriteLine(stack.Pop());
+
+Console.WriteLine(stack.Capacity);
+
+Console.WriteLine(stack.Count);
