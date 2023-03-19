972f846b393c917d3233b77828fe054b4d326548
Nikita Strekalov (laptop) <nikita.strekalov123@gmail.com>
2023-03-19 19:17:46 +0400

    add initial files

---
+++ PowerCollections/PowerCollections.Tests/StackTests.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PowerCollections.Tests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void Constructor_create_stack_with_zero_count_and_requested_capacity()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual(1, stack.Capacity);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),"Param bad format.Stack size must not be less than or equal to 0")]
+        public void Constructor_CheckingForAnExceptionWithConstructorCreateStackWithSizeIsZero()
+        {
+            Stack<int> stack = new Stack<int>(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Param bad format.Stack size must not be less than or equal to 0")]
+        public void Constructor_CheckingForAnExceptionWithConstructorCreateStackWithSizeIsLessThanZero()
+        {
+            Stack<int> stack = new Stack<int>(-1);
+        }
+
+        [TestMethod]
+        public void Push_ItemInStackСheckingThatTopItemEqualsTheAddedValueAndCountIsEqualToOne()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Top());
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException),"Overflow stack")]
+        public void Push_ExceptionMessageIfPushItemsGreaterThanSizeStack()
+        {
+            Stack<string> stack = new Stack<string>(2);
+            stack.Push("test");
+            stack.Push("test");
+            stack.Push("test");
+;
+        }
+
+        [TestMethod]
+        public void Pop_ItemInStackСheckingThatPopItemEqualsTheAddedValueAndCountIsEqualToZero()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Pop());
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Stack is empty")]
+        public void Pop_ExceptionMessagePopItemIfStackIsEmpty()
+        {
+            Stack<string> stack = new Stack<string>(2);
+            stack.Pop();
+        }
+
+        [TestMethod]
+        public void Top_ItemInStackCheckingThatTopItemEqualsTheAddedValue()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Top());
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Stack is empty")]
+        public void Top_ExceptionMessagePopItemIfStackIsEmpty()
+        {
+            Stack<string> stack = new Stack<string>(2);
+            stack.Top();
+        }
+
+        [TestMethod]
+        public void Enumerator_GetEnumeratorFromStackIsEmpty()
+        {
+            Stack<int> stack = new Stack<int>(2);
+            IEnumerator enumerator = stack.GetEnumerator();
+            Assert.AreEqual(false,enumerator.MoveNext());
+            Assert.AreEqual(0, enumerator.Current);
+        }
+        [TestMethod]
+        public void Enumerator_GetEnumeratorFromStackIsNotEmpty()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            IEnumerator enumerator = stack.GetEnumerator();
+            stack.Push(1);
+            enumerator.MoveNext();
+            Assert.AreEqual(false, enumerator.MoveNext());
+        }
+        [TestMethod]
+        public void Enumerator_GetEnumeratorGoFromTopToBottom()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            IEnumerator enumerator = stack.GetEnumerator();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            enumerator.MoveNext();
+            Assert.AreEqual(3, enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(2, enumerator.Current);
+            enumerator.MoveNext();
+            Assert.AreEqual(1, enumerator.Current);
+        }
+    }
+}
---
+++ PowerCollections/PowerCollections/Stack.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace PowerCollections
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private T[] items;
+
+        private int count;
+
+        public int Capacity { get; }
+        public int Count
+        {
+            get { return count; }
+        }
+        public Stack(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Param bad format.Stack size must not be less than or equal to 0");
+            }
+            Capacity = size;
+            items = new T[size];
+        }
+
+        public bool IsEmpty {
+            get { return count == 0; } 
+        }
+
+        public void Push (T item)
+        {
+            if (count == items.Length)
+            {
+                throw new InvalidOperationException("Overflow stack");
+            }
+            items[count++] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return items[--count];
+        }
+
+        public T Top()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return items[count - 1];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+
+            if (IsEmpty)
+            {
+                yield break;
+            }
+            for (int i = count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+                
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
